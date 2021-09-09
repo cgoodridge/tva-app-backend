34d696de7364701efb6f97514522e76676f8eb7d
cgoodridge <cag45@live.com>
2021-09-09 02:20:36 -0400

    Updated index.html

--- src/server.js
+++ src/server.js
@@ -176,7 +176,7 @@ app.get('*', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', '/index.html'));
     });
 }
 else {
