66bd076a3a4624bd51e1cce268f4db889099ab41
cgoodridge <cag45@live.com>
2022-03-17 18:25:26 -0400

    Sacred timeline temporarily offline

--- client/src/pages/NexusEventList.js
+++ client/src/pages/NexusEventList.js
@@ -4,6 +4,7 @@ import threatContent from './threat-content';
 import EventList from '../components/EventList';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import { Box, Typography } from '@material-ui/core';
 
 const NexusEventListPage = () => {
 
@@ -14,32 +15,32 @@ const NexusEventListPage = () => {
     // initialize the canvas context
     useEffect(() => {
         // dynamically assign the width and height to canvas
-        const canvasEle = canvas.current;
-        canvasEle.width = canvasEle.clientWidth;
-        canvasEle.height = canvasEle.clientHeight;
+        // const canvasEle = canvas.current;
+        // canvasEle.width = canvasEle.clientWidth;
+        // canvasEle.height = canvasEle.clientHeight;
 
         // get context of the canvas
-        ctx = canvasEle.getContext("2d");
+        // ctx = canvasEle.getContext("2d");
     }, []);
 
     useEffect(() => {
 
-        const r2Info = { x: 100, y: 100, w: 80, h: 150 };
-        drawRect(r2Info);
+        // const r2Info = { x: 100, y: 100, w: 80, h: 150 };
+        // drawRect(r2Info);
 
     }, []);
 
     // draw rectangle
-    const drawRect = (info, style = {}) => {
-        const { x, y, w, h } = info;
-        const { borderColor = 'black', borderWidth = 1 } = style;
+    // const drawRect = (info, style = {}) => {
+    //     const { x, y, w, h } = info;
+    //     const { borderColor = 'black', borderWidth = 1 } = style;
 
-        ctx.beginPath();
-        ctx.strokeStyle = borderColor;
-        ctx.lineWidth = borderWidth;
-        ctx.rect(x, y, w, h);
-        ctx.stroke();
-    }
+    //     ctx.beginPath();
+    //     ctx.strokeStyle = borderColor;
+    //     ctx.lineWidth = borderWidth;
+    //     ctx.rect(x, y, w, h);
+    //     ctx.stroke();
+    // }
 
     useEffect(() => {
 
@@ -63,7 +64,7 @@ const NexusEventListPage = () => {
                     </Grid>
                     <Grid item xs={12} sm={6} container>
                         <Grid item xs={12} sm={7}>
-                            <h2 className="pageHeader">Multiple Nexus Events Detected!</h2>
+                            {/* <h2 className="pageHeader">Multiple Nexus Events Detected!</h2> */}
                         </Grid>
                         <Grid item xs={12} sm={5}>
                             <div className="redLight"></div>
@@ -78,7 +79,13 @@ const NexusEventListPage = () => {
                 <source src="./video/sacred_timeline.mp4" type="video/mp4"></source>
             </video>
         </div> */}
-            <canvas ref={canvas}></canvas>
+            {/* <canvas ref={canvas}></canvas> */}
+            
+                <Typography variant="h5" className="errorMessage">
+                    Howdy y'all, the sacred timeline suffered some nasty damage, but our minutemen are working their very hardest to get it back up and running!
+                </Typography>
+            
+
 
             <Container>
                 <EventList nexusEvents={nexusEvents} />
